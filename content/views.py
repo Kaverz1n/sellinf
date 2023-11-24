1452c8ee141ea99ea7aa75844d1257c22022bd08
Dmitry <dima.captan@yandex.ru>
2023-11-24 15:45:26 +0300

    added IndexView

--- content/views.py
+++ content/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    '''
+    Index page view
+    '''
+    template_name = 'content/index.html'
