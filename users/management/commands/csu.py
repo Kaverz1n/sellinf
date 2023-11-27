2fab41e897ec6245951cf57b1d5fc243d46eab0b
Dmitry <dima.captan@yandex.ru>
2023-11-27 19:58:25 +0300

    added a command to create a superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,26 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    '''
+    The command to create a superuser
+    '''
+
+    def handle(self, *args, **options) -> None:
+        try:
+            user = User.objects.create(
+                phone='+9999',
+                nickname='Admin',
+                is_upgraded=True,
+                is_staff=True,
+                is_superuser=True,
+            )
+
+            user.set_password('Admin')
+            user.save()
+
+            self.stdout.write('Superuser created successfully')
+        except Exception as e:
+            self.stderr.write(f'Error creating superuser: {e}')
