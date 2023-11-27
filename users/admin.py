b7cef2aa90271a66ee655bc2ae81f1e79ddde0e1
Dmitry <dima.captan@yandex.ru>
2023-11-27 19:49:51 +0300

    added UserAdmin and ConfirmationCodeAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User, ConfirmationCode
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'nickname', 'is_upgraded',)
+    list_filter = ('is_upgraded',)
+    search_fields = ('phone', 'nickname',)
+
+
+@admin.register(ConfirmationCode)
+class ConfirmationCodeAdmin(admin.ModelAdmin):
+    list_display = ('display_user_phone', 'code',)
+
+    def display_user_phone(self, obj):
+        return obj.user.phone
