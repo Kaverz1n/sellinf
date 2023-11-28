c2720514e4c956d01312be584edb24e8fb941378
Dmitry <dima.captan@yandex.ru>
2023-11-28 19:56:46 +0300

    added a path authorization

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import UserRegisterView, ConfirmationCodeView
+from users.views import UserRegisterView, ConfirmationCodeView, AuthorizationView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('registration/', UserRegisterView.as_view(), name='registration'),
     path('confirmation/<int:user_id>/', ConfirmationCodeView.as_view(), name='confirmation_account'),
+    path('authorization/', AuthorizationView.as_view(), name='authorization'),
 ]
