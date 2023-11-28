ed34e81b2fd7b205a24a526caaf463559faba468
Dmitry <dima.captan@yandex.ru>
2023-11-28 20:00:22 +0300

    added a func code_generation

---
+++ users/services.py
@@ -0,0 +1,22 @@
+import random
+
+import django
+
+from users.models import ConfirmationCode, User
+from users.tasks import send_confirmation_code
+
+
+def code_generation(user: User) -> None:
+    '''
+    Create a confirmation code
+    '''
+    code = int(''.join([str(random.randint(1, 9)) for _ in range(4)]))
+
+    try:
+        ConfirmationCode.objects.create(user=user, code=code)
+    except django.db.utils.IntegrityError:
+        users_code = ConfirmationCode.objects.get(user=user)
+        users_code.code = code
+        users_code.save()
+
+    send_confirmation_code.delay(user.phone)
