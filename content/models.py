2021b06e45a1a252aa79733044c1e7d413bbc45c
Dmitry <dima.captan@yandex.ru>
2023-11-28 19:54:57 +0300

    added model Content

--- content/models.py
+++ content/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Content(models.Model):
+    '''
+    Content model class
+    '''
+    TYPE_CHOICES = (
+        ('premium', 'Premium'),
+        ('free', 'Free'),
+    )
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='owner')
+    title = models.CharField(max_length=100, verbose_name='title', unique=True)
+    content = models.TextField(verbose_name='content')
+    type = models.CharField(max_length=7, choices=TYPE_CHOICES, verbose_name='type')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated at')
+    views = models.IntegerField(default=0, verbose_name='views')
+    is_published = models.BooleanField(default=False, verbose_name='published')
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'content'
+        verbose_name_plural = 'contents'
