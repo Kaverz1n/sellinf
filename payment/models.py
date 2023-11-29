f1dffefbee1785f551283fb2c48441c055f0ede6
Dmitry <dima.captan@yandex.ru>
2023-11-29 20:30:02 +0300

    added Payment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Payment(models.Model):
+    '''
+    Payment model
+    '''
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='user')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='date')
+    checkout_id = models.CharField(max_length=255, verbose_name='checkout id', unique=True)
+
+    def __str__(self) -> str:
+        return f'Payment {self.user.phone}'
+
+    class Meta:
+        verbose_name = 'payment'
+        verbose_name_plural = 'payment'
