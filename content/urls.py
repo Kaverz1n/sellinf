ade13791744abb46afc65988a4fa476d04376821
Dmitry <dima.captan@yandex.ru>
2023-11-30 20:31:02 +0300

    added new paths

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,6 @@
 from content.apps import ContentConfig
 from content.views import IndexView, ContentListView, FoundContentListView, AboutTemplateView, UpgradeTemplateView, \
-    ContentDetailView
+    ContentDetailView, ContentCreateView, UserContentListView
 
 from django.urls import path
 
@@ -10,7 +10,9 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('contents/', ContentListView.as_view(), name='content_list'),
     path('contents/<str:search_query>/', FoundContentListView.as_view(), name='found_content_list'),
+    path('content/<int:pk>/publications/', UserContentListView.as_view(), name='user_content_list'),
     path('content/<int:pk>/', ContentDetailView.as_view(), name='content_detail'),
+    path('content/create/', ContentCreateView.as_view(), name='content_create'),
     path('about/', AboutTemplateView.as_view(), name='about'),
     path('upgrade/', UpgradeTemplateView.as_view(), name='upgrade'),
 ]
