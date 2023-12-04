7691b32d459525d0ad879ba64a7d8bf1d76c7ac9
Dmitry <dima.captan@yandex.ru>
2023-12-04 20:26:41 +0300

    added new paths

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,9 @@
 from content.apps import ContentConfig
-from content.views import IndexView, ContentListView, FoundContentListView, AboutTemplateView, UpgradeTemplateView, \
-    ContentDetailView, ContentCreateView, UserContentListView
+from content.views import (
+    IndexView, ContentListView, FoundContentListView, AboutTemplateView, UpgradeTemplateView,
+    ContentDetailView, ContentCreateView, UserContentListView, ContentUpdateView, ContentDeleteView,
+    ContentPublishStatusView, ModeratorContentListView
+)
 
 from django.urls import path
 
@@ -11,8 +14,12 @@ urlpatterns = [
     path('contents/', ContentListView.as_view(), name='content_list'),
     path('contents/<str:search_query>/', FoundContentListView.as_view(), name='found_content_list'),
     path('content/<int:pk>/publications/', UserContentListView.as_view(), name='user_content_list'),
+    path('content/check/', ModeratorContentListView.as_view(), name='moderator_content_list'),
     path('content/<int:pk>/', ContentDetailView.as_view(), name='content_detail'),
     path('content/create/', ContentCreateView.as_view(), name='content_create'),
+    path('content/<int:pk>/update/', ContentUpdateView.as_view(), name='content_update'),
+    path('content/<int:pk>/delete/', ContentDeleteView.as_view(), name='content_delete'),
+    path('content/<int:pk>/publish/', ContentPublishStatusView.as_view(), name='content_publish_status'),
     path('about/', AboutTemplateView.as_view(), name='about'),
     path('upgrade/', UpgradeTemplateView.as_view(), name='upgrade'),
 ]
