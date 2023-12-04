654e0944424b4fadec540cd5dd30907d9337d200
Dmitry <dima.captan@yandex.ru>
2023-12-04 20:27:58 +0300

    added permissions, ModeratorContentListView, ContentPublishStatusView, ContentUpdateView and ContentDeleteView

--- content/views.py
+++ content/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
 from django.db.models import QuerySet, Q
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -24,13 +24,13 @@ class IndexView(UserPassesTestMixin, generic.TemplateView):
         return redirect(reverse('content:content_list'))
 
 
-class ContentCreateView(LoginRequiredMixin, UserPassesTestMixin, generic.CreateView):
+class ContentCreateView(LoginRequiredMixin, PermissionRequiredMixin, generic.CreateView):
     '''
     Content create view
     '''
     model = Content
     form_class = ContentForm
-    extra_context = {'title': 'Create content'}
+    permission_required = 'content.add_content'
 
     def form_valid(self, form) -> HttpResponse:
         if form.is_valid():
@@ -39,6 +39,14 @@ class ContentCreateView(LoginRequiredMixin, UserPassesTestMixin, generic.CreateV
             self.object.save()
 
             return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create new content'
+        context['text'] = 'CREATING'
+        context['button_text'] = 'CREATE'
+
+        return context
 
     def get_success_url(self) -> str:
         messages.info(self.request, 'Content will be published after verification by a moderator!')
@@ -115,6 +123,7 @@ class UserContentListView(UserPassesTestMixin, ContentListView):
     User content list view
     '''
     template_name = 'content/user_content_list.html'
+    paginate_by = 5
 
     def get_queryset(self, *args, **kwargs) -> QuerySet:
         queryset = Content.objects.filter(owner=self.request.user)
@@ -138,6 +147,50 @@ class UserContentListView(UserPassesTestMixin, ContentListView):
         return redirect(reverse('content:upgrade'))
 
 
+class ModeratorContentListView(UserPassesTestMixin, ContentListView):
+    '''
+    Moderator content list view
+    '''
+    template_name = 'content/moderator_content_list.html'
+    paginate_by = 5
+
+    def get_queryset(self, *args, **kwargs) -> QuerySet:
+        queryset = Content.objects.filter(is_published=False)
+
+        return queryset
+
+    def get_context_data(self, *args, **kwargs) -> dict:
+        context = super().get_context_data(*args, **kwargs)
+        context['title'] = f'Checking content'
+        context['form'] = ''
+
+        return context
+
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        pass
+
+    def test_func(self) -> bool:
+        group = self.request.user.groups.filter(name='moderator').exists()
+
+        return group
+
+    def handle_no_permission(self) -> HttpResponse:
+        return redirect(reverse('content:upgrade'))
+
+
+class ContentPublishStatusView(generic.View):
+    '''
+    Class for change publish status of content
+    '''
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        self.object = Content.objects.get(pk=self.kwargs['pk'])
+        self.object.is_published = True
+        self.object.save()
+
+        return redirect(reverse('content:moderator_content_list'))
+
+
 class ContentDetailView(LoginRequiredMixin, generic.DetailView):
     '''
     Content detail view
@@ -156,6 +209,74 @@ class ContentDetailView(LoginRequiredMixin, generic.DetailView):
         context['title'] = self.object.title
 
         return context
+
+
+class ContentUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    '''
+    Content update view
+    '''
+    model = Content
+    form_class = ContentForm
+    permission_required = 'content.change_content'
+
+    def form_valid(self, form) -> HttpResponse:
+        if form.is_valid():
+            self.object = form.save()
+            self.object.is_published = False
+            self.object.save()
+
+            return super().form_valid(form)
+
+    def get_success_url(self) -> str:
+        messages.info(self.request, 'Content will be updated after verification by a moderator!')
+        return reverse('content:content_list')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Update {self.object.title}'
+        context['text'] = 'UPDATING'
+        context['button_text'] = 'UPDATE'
+
+        return context
+
+    def test_func(self) -> bool:
+        return self.request.user == self.get_object().owner
+
+    def handle_no_permission(self) -> HttpResponse:
+        return redirect(reverse('content:upgrade'))
+
+
+class ContentDeleteView(LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    '''
+    Content delete view
+    '''
+    model = Content
+    permission_required = 'content.delete_content'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Delete {self.object.title}'
+
+        return context
+
+    def get_success_url(self) -> str:
+        messages.success(self.request, 'Content have been successfully deleted!')
+
+        moderator = self.request.user.groups.filter(name='moderator')
+
+        if moderator:
+            return reverse('content:moderator_content_list')
+
+        return reverse('content:content_list')
+
+    def test_func(self) -> bool:
+        owner = self.request.user == self.get_object().owner
+        moderator = self.request.user.groups.filter(name='moderator') and not self.get_object().is_published
+
+        return owner or moderator
+
+    def handle_no_permission(self) -> HttpResponse:
+        return redirect(reverse('content:upgrade'))
 
 
 class AboutTemplateView(LoginRequiredMixin, generic.TemplateView):
